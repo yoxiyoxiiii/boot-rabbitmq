e6db2fc3304b5a91ddcf46a0c2eb68339971b2fc
846814595@qq.com <846814595@qq.com>
2019-03-07 11:54:22 +0800

    direct

--- src/main/java/com/example/bootrabbitmq/direct/DirectQueue.java
+++ src/main/java/com/example/bootrabbitmq/direct/DirectQueue.java
@@ -1,7 +1,6 @@
 package com.example.bootrabbitmq.direct;
 
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +21,48 @@ public class DirectQueue {
         return "success";
     }
 
+    @GetMapping("send/direct")
+    public String sendirect() {
+        //声明了直连exchange ，必须指定
+        amqpTemplate.convertAndSend("directExchange","hello-direct","hello-direct");
+        return "success";
+    }
+
     //声明一个队列
     @Bean
     public Queue queue() {
         return new Queue("hello");
     }
 
+    /**
+     * 声明一个直连队列
+     * @return
+     */
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange("directExchange");
+    }
+
+    /**
+     * 队列和 直连exchange 绑定
+     * @param directExchange
+     * @param queue
+     * @return
+     */
+    @Bean
+    public Binding binding(DirectExchange directExchange,
+                           Queue queue) {
+
+        return BindingBuilder.bind(queue).to(directExchange).with("hello-direct");
+
+    }
+
+
+    /**
+     * 消息消费者
+     * @param msg
+     * @RabbitListener(queues = "hello") 监听的队列
+     */
     @RabbitListener(queues = "hello")
     @RabbitHandler
     public void revc(String msg) {
