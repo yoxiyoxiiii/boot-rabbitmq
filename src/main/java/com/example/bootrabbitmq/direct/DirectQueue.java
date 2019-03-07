4b21fec176fff03661fe18695873abd6d732e60e
846814595@qq.com <846814595@qq.com>
2019-03-07 17:33:56 +0800

    fanout

--- src/main/java/com/example/bootrabbitmq/direct/DirectQueue.java
+++ src/main/java/com/example/bootrabbitmq/direct/DirectQueue.java
@@ -1,5 +1,6 @@
 package com.example.bootrabbitmq.direct;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class DirectQueue {
@@ -27,15 +30,48 @@ public class DirectQueue {
     @GetMapping("send/direct")
     public String sendirect() {
         //声明了直连exchange ，必须指定
-        amqpTemplate.convertAndSend("directExchange","hello-directx","hello-direct");
+        amqpTemplate.convertAndSend("directExchange","hello-direct","hello-direct");
         return "success";
+    }
+
+
+
+    /**
+     * 声明死信交换机
+     * @return
+     */
+    @Bean
+    public DirectExchange deadDirectExchange() {
+        return new DirectExchange("deadDirectExchange");
+    }
+
+    /**
+     * 声明死信队列
+     * @return
+     */
+    @Bean
+    public Queue deadQueue() {
+        return new Queue("deadQueue");
+    }
+
+    @Bean
+    public Binding deadBinding (Queue deadQueue,
+                                DirectExchange deadDirectExchange) {
+        return BindingBuilder.bind(deadQueue).to(deadDirectExchange).with("dead");
     }
 
     //声明一个队列
     @Bean
     public Queue queue() {
-        return new Queue("hello");
+        Map<String,Object> args = new HashMap<>();
+        //为该队列设置死信exchange
+        args.put("x-dead-letter-exchange","deadDirectExchange");
+        args.put("x-dead-letter-routing-key","dead");
+        return new Queue("hello",true,false,false,args);
     }
+
+
+
 
     /**
      * 声明一个直连队列
@@ -70,9 +106,12 @@ public class DirectQueue {
     @RabbitHandler
     public void revc(String msg, Channel channel, Message message) throws IOException {
         System.out.println(msg);
-        channel.basicQos(1);
+//        channel.basicQos(1);
+//        throw new RuntimeException("sss");
         //手动确认ack
-        channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+//        channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+//        channel.basicReject(message.getMessageProperties().getDeliveryTag(),false);
+        int i = 1/0;
     }
 
 }
